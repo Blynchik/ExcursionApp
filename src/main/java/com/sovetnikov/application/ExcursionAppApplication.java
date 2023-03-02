34fead4379466a58a09baec74c5e09145d2b6c4a
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-02 08:43:13 +0300

    first commit

---
+++ src/main/java/com/sovetnikov/application/ExcursionAppApplication.java
@@ -0,0 +1,13 @@
+package com.sovetnikov.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcursionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcursionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sovetnikov/application/ExcursionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sovetnikov.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcursionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
