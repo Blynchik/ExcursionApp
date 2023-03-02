492ccdf2fc3cce6980d386677c8ab7aa92204bd2
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-02 12:04:13 +0300

    simple user service added

---
+++ src/main/java/com/sovetnikov/application/service/UserService.java
@@ -0,0 +1,51 @@
+package com.sovetnikov.application.service;
+
+import com.sovetnikov.application.model.User;
+import com.sovetnikov.application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void create(User user) {
+        userRepository.save(user);
+    }
+
+    public Optional<User> get(int id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @Transactional
+    public void update(User updatedUser, int id) {
+        User user = userRepository.getReferenceById(id);
+
+        user.setEmail(updatedUser.getEmail());
+        user.setPhoneNumber(updatedUser.getPhoneNumber());
+        user.setName(updatedUser.getName());
+
+        userRepository.save(updatedUser);
+    }
+
+    @Transactional
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+}
