bf4881909bab31bf1a9194648b3379b89eb30ce3
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-03 15:35:53 +0300

    authorization siplified

--- src/main/java/com/sovetnikov/application/config/SecurityConfig.java
+++ src/main/java/com/sovetnikov/application/config/SecurityConfig.java
@@ -1,19 +1,61 @@
 package com.sovetnikov.application.config;
 
+
+import com.sovetnikov.application.model.User;
+import com.sovetnikov.application.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import com.sovetnikov.application.security.AuthProvider;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Collections;
+import java.util.Optional;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Bean
-    protected AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> {
+            Optional<User> optionalUser = userRepository.findByEmail(email);
+
+            if(optionalUser.isEmpty()){
+                throw new UsernameNotFoundException("Пользователь не существует");
+            }
+
+            return new org.springframework.security.core.userdetails.User(
+                    optionalUser.get().getEmail(),
+                    optionalUser.get().getPassword(),
+                    Collections.emptyList());
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/api/users", true);
+        return http.build();
     }
 }
