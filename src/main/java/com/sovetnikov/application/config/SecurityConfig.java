325ceab37850103245cad13d9929a6078683177f
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-04 12:29:45 +0300

    filterChain improved

--- src/main/java/com/sovetnikov/application/config/SecurityConfig.java
+++ src/main/java/com/sovetnikov/application/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.sovetnikov.application.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -51,11 +52,13 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
+        http.csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/login").anonymous()
                 .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .defaultSuccessUrl("/api/users", true);
+                .formLogin().defaultSuccessUrl("/api/users", true);
+
         return http.build();
     }
 }
