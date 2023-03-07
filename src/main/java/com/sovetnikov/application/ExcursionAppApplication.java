978b3411385c82064d016df69c91b4804ca73469
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-07 09:54:45 +0300

    back to postgres

--- src/main/java/com/sovetnikov/application/ExcursionAppApplication.java
+++ src/main/java/com/sovetnikov/application/ExcursionAppApplication.java
@@ -9,22 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExcursionAppApplication implements ApplicationRunner {
-
-	private final UserService userService;
-
-	@Autowired
-	public ExcursionAppApplication(UserService userService) {
-		this.userService = userService;
-	}
-
+public class ExcursionAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExcursionAppApplication.class, args);
-	}
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		userService.create(new User("User", "user@yandex.ru", "9006005040", "password"));
-		userService.create(new User("Admin", "admin@mail.ru", "9007008060", "password"));
 	}
 }
