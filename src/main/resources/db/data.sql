bc40fad4355b62eae36262e59c60b29fc86c4de1
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-11 09:40:14 +0300

    added tables for excursion

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,3 +1,7 @@
 insert into users(name, email, phone_number, password, role)
 values ('User', 'user@yandex.ru', '9006005040', '{noop}password', 'ROLE_USER'),
        ('Admin', 'admin@mail.ru', '9007008060', '{noop}password', 'ROLE_ADMIN');
+
+insert into excursion(name, date, description, price)
+values ('Памуккале', current_date + 10, '', '450'),
+       ('Мира', current_date + 13, '', '350');
