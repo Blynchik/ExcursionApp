3ea99f889fd80a84268b705e9070b4b99116256a
Vadim Sovetnikov <vadimsovetnikov@mail.ru>
2023-03-11 17:20:52 +0300

    table for comment added

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -3,10 +3,14 @@ values ('User', 'user@yandex.ru', '9006005040', '{noop}password', 'ROLE_USER'),
        ('Admin', 'admin@mail.ru', '9007008060', '{noop}password', 'ROLE_ADMIN');
 
 insert into excursion(name, date, description, price)
-values ('Памуккале', current_date + 10, '', '450'),
-       ('Мира', current_date + 13, '', '350');
+values ('Памуккале', current_timestamp, '', '450'),
+       ('Мира', current_timestamp, '', '350');
 
 insert into users_excursion(users_id, excursion_id)
 values (1, 1),
        (1, 2),
        (2, 1);
+
+insert into comment(message, created_at, users_id, excursion_id)
+values ('Понравилось', current_date + 10, 1, 1),
+       ('Не понравилось', current_date + 13, 2, 2);
